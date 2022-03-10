360e48f301649974763d1abd9d183ef15843d679
Leonardo <45762153+leonardojung2001@users.noreply.github.com>
2022-03-10 15:41:30 -0300

    Update blackbull.py
    Language support!

--- blackbull.py
+++ blackbull.py
@@ -10,12 +10,12 @@ for key in arquivo:
 	encrypted_word = hashlib.md5(key.encode())
 
 	if encrypted_word.hexdigest() == sys.argv[1]:
-		print("o hash " + str(encrypted_word.hexdigest()) + " tem a chave " + key)
+		print("The hash " + str(encrypted_word.hexdigest()) + " has de key: " + key)
 		exit()
 	else:
 		print(encrypted_word.hexdigest() + " / " + key)
 		
-print("Chave não encontrada!")
+print("Key not found!")
 
 
 
